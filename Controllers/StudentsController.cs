a81bc3c7c36db5d9666d7f2534c76b9a8425d454
Michael <Michael@DESKTOP-6395OH7>
2023-11-13 14:27:27 +0300

    Add project files.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetStudents()
+        {
+            string[] students = { "Henry", "Eric", "Alex" };
+            if (students.Any())
+                return NotFound();
+            return Ok(students);
+
+        }
+
+        /*[HttpPost]
+        public ActionResult CreateStudents()
+        {
+            string[] students = { "Henry", "Eric", "Alex" };
+            
+        }*/
+
+        [HttpDelete]
+        public ActionResult DeleteStudents()
+        {
+            bool badThingshappned = false;
+            if (badThingshappned)
+                return BadRequest();
+            return NoContent();
+        }
+    }
+}
